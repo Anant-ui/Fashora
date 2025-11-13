af9d707c45a4ff7b39f107ad367eaceac59245ff
Anant-ui <anantpatairiya@gmail.com>
2025-11-13 18:51:00 +0530

    djksk

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 // src/App.jsx
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar";
-import Hero from "./Components/Hero";
-import Featured from "./Components/Featured";
-import About from "./Components/About";
-import Contact from "./Components/Contact";
-import Cart from "./Components/Cart";
+import Navbar from "./Components/Navbar.jsx";
+import Hero from "./Components/Hero.jsx";
+import Featured from "./Components/Featured.jsx";
+import About from "./Components/About.jsx";
+import Contact from "./Components/Contact.jsx";
+import Cart from "./Components/Cart.jsx";
+
 
 export default function App() {
   return (
